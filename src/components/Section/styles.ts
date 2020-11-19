c8be1f39b5e34f9ff28f0ff964c0c5747ebd859a
Renato Hideki Osaka <renato.osaka@hotmail.com>
2020-11-19 13:27:39 -0300

    chore: adjustments

--- src/components/Section/styles.ts
+++ src/components/Section/styles.ts
@@ -34,9 +34,7 @@ export const Container = styled.div`
 
 export const Content = styled.div<ContentProps>`
   width: 100%;
-
-  max-width: 1120px;
-  margin: 0 auto;
+  height: 100vh;
 
   display: flex;
   align-items: stretch;
@@ -47,7 +45,10 @@ export const Content = styled.div<ContentProps>`
     background: url(${props => props.background_url}) no-repeat center;
     background-size: cover;
     border-radius: 8px;
-    box-shadow: 2px 0 4px rgba(0, 0, 0, 0.25);
+    box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+      0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
+      0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086),
+      0 100px 80px rgba(0, 0, 0, 0.12);
   }
 
   .content {
